ee52bb8c7f894f4993c511c3d16223017bcd4ba8
Diego Axel Contreras Guerrero <diegoaxelcontreras13@gmail.com>
2024-10-16 08:39:49 -0600

    Act

--- ProyectU2/Cinepolis/src/menu/Menu.java
+++ ProyectU2/Cinepolis/src/menu/Menu.java
@@ -13,6 +13,82 @@ public class Menu {
     private final Cine cine = new Cine();
 
     //-------------- Métodos para mostrar -----------------
+    private void mostrarMenuAdmin(Administrador admin) {
+        int respuesta = 0;
 
+        while (respuesta != 12) {
+            System.out.println("Buen dia " + admin.nombre + "-" + admin.apellido);
+            System.out.println("""
+                    1.- Registrar una pelicula
+                    2.- 
+                  
+                    12.- Salir""");
+            System.out.print("Elija una opción: ");
+            respuesta = scanner.nextInt();
+
+            switch (respuesta) {
+                case 1:
+                    System.out.println("Registro de una pelicula");
+                    String id = cine.generarIdPelicula();
+                    System.out.print("Ingrese el titulo: ");
+                    String titulo = scanner.nextLine();
+                    System.out.print("Ingrese la duración: ");
+                    int duracion = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.print("Ingrese el genero: ");
+                    String genero = scanner.nextLine();
+                    System.out.print("Ingrese la clasificación: ");
+                    String clasificacion = scanner.nextLine();
+                    System.out.print("Ingrese la sinopsis: ");
+                    String sinopsis = scanner.nextLine();
+
+                    System.out.print("Seleccione el estado de la pelicula:" +
+                            "\n1. Estado Actual " +
+                            "2. Estado Proximamente");
+                    System.out.print("Selección: ");
+                    int estado = scanner.nextInt();
+                    scanner.nextLine();
+                    Pelicula pelicula = null;
+                    switch (estado){
+                        case 1:
+                            pelicula = new Pelicula(id, titulo, duracion, genero, clasificacion, sinopsis, EstadoPelicula.ACTUAL);
+                            break;
+                        case 2:
+                            pelicula = new Pelicula(id, titulo, duracion, genero, clasificacion, sinopsis, EstadoPelicula.PROXIMAMENTE);
+                            break;
+                    }
+
+                    boolean band = true;
+                    LocalTime funcion = null;
+                    do {
+                        System.out.println("Ingrese la hora y los minutos de una función: ");
+                        System.out.print("Ingrese la hora: ");
+                        int hora = scanner.nextInt();
+                        System.out.print("Ingrese los minutos: ");
+                        int minutos = scanner.nextInt();
+                        scanner.nextLine();
+
+                        funcion = LocalTime.of(hora, minutos);
+                        pelicula.agregarFuncion(funcion);
+
+                        System.out.print("¿Desea Agregar otra función? S/N");
+                        String r = scanner.nextLine().charAt(0) + "";
+                        if (!r.toLowerCase().equals("s")) {
+                            band = false;
+                        }
+                    }while(band);
+
+                    cine.registrarPelicula(pelicula);
+                    System.out.println("Registro Exitoso");
+                    break;
+                case 2:
+                    break;
+                case 12:
+                    System.out.println("\n-----Adiosito-----\n");
+                    return;
+            }
+        }
+        scanner.close();
+    }
 
 }
