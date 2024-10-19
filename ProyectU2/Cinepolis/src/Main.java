372c8a7d31c85a15850a08a9ac453b6a749b5088
Diego Axel Contreras Guerrero <diegoaxelcontreras13@gmail.com>
2024-10-19 09:40:00 -0600

    .

--- ProyectU2/Cinepolis/src/Main.java
+++ ProyectU2/Cinepolis/src/Main.java
@@ -1,15 +1,9 @@
-
 import cine.Cine;
 import menu.Menu;
-import usuarios.administrador.Administrador;
-import utils.Rol;
-//Rol y usuario  importandos para probar al igual que la creacion de un admin
 
 public class Main {
     public static void main(String[] args) {
         Menu menu = new Menu();
-
-        Administrador admin = new Administrador("1", "Admin", "Apellido", "1234567890", "contrasenia", Rol.ADMINISTRADOR);
 
         //menu.login();
         Cine cine = new Cine();
